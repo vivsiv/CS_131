7bd339b0751be814055655a06ebf3815a82933f6
Vivek <vivek.a.sivakumar@gmail.com>
2016-01-12 02:25:18 -0800

    filter_rules and filter_blind_alleys

--- hw1.ml
+++ hw1.ml
@@ -162,31 +162,90 @@ if debug then Printf.printf "find_all_terminating_symbols rules: %B\n" (find_all
 if debug then Printf.printf "find_all_terminating_symbols rules2: %B\n" (find_all_terminating_symbols(rules2) = [Expr;Binop]);;
 if debug then Printf.printf "find_all_terminating_symbols rules3: %B\n" (find_all_terminating_symbols(rules3) = [Binop;Incrop;Expr]);;
 
-(* let rec filter_rules rules term_symbols = 
+let rec filter_rules rules terminal_symbols = 
 	match rules with
 		| [] -> []
-		| r::t -> 
-			if rule_terminates r term_symbols
-			then r::(filter_rules t term_symbols)
-			else filter_rules t term_symbols
+		| (nt_symbol,rh_side)::rules_tail -> 
+			if contains terminal_symbols nt_symbol
+			then (nt_symbol,rh_side)::(filter_rules rules_tail terminal_symbols)
+			else filter_rules rules_tail terminal_symbols
 ;;	
 
 let filter_blind_alleys g =
 	match g with
-		| (start_sym,rules) -> (start_sym, filter_rules rules (find_terminating_symbols [] rules))
+		| (start_sym,rules) -> (start_sym, filter_rules rules (find_all_terminating_symbols rules))
 ;;
 
-let test_grammar = Expr, rules;;
-let test_filtered_grammar = Expr, [Binop, [N Expr]; Expr, [T "("; N Expr; T ")"]; Expr, [T "5"]; Incrop, [T "++"]];;
+type awksub_nonterminals =
+  | Expr | Lvalue | Incrop | Binop | Num
+;;
 
-if debug then Printf.printf "filter_blind_alleys rules: %B\n" ((filter_blind_alleys test_grammar) = test_filtered_grammar);;
+let awksub_rules =
+   [Expr, [T"("; N Expr; T")"];
+    Expr, [N Num];
+    Expr, [N Expr; N Binop; N Expr];
+    Expr, [N Lvalue];
+    Expr, [N Incrop; N Lvalue];
+    Expr, [N Lvalue; N Incrop];
+    Lvalue, [T"$"; N Expr];
+    Incrop, [T"++"];
+    Incrop, [T"--"];
+    Binop, [T"+"];
+    Binop, [T"-"];
+    Num, [T"0"];
+    Num, [T"1"];
+    Num, [T"2"];
+    Num, [T"3"];
+    Num, [T"4"];
+    Num, [T"5"];
+    Num, [T"6"];
+    Num, [T"7"];
+    Num, [T"8"];
+    Num, [T"9"]]
+;;
 
-let test_grammar_2 = Expr, rules2;;
-let test_filtered_grammar_2 = Expr, rules2;;
-if debug then Printf.printf "filter_blind_alleys rules2: %B\n" ((filter_blind_alleys test_grammar_2) = test_filtered_grammar_2);; *)
+let awksub_grammar = Expr, awksub_rules;;
+
+let awksub_test0 =
+  filter_blind_alleys awksub_grammar = awksub_grammar;;
+
+let awksub_test1 =
+  filter_blind_alleys (Expr, List.tl awksub_rules) = (Expr, List.tl awksub_rules);;
+
+let awksub_test2 =
+  filter_blind_alleys (Expr,
+      [Expr, [N Num];
+       Expr, [N Lvalue];
+       Expr, [N Expr; N Lvalue];
+       Expr, [N Lvalue; N Expr];
+       Expr, [N Expr; N Binop; N Expr];
+       Lvalue, [N Lvalue; N Expr];
+       Lvalue, [N Expr; N Lvalue];
+       Lvalue, [N Incrop; N Lvalue];
+       Lvalue, [N Lvalue; N Incrop];
+       Incrop, [T"++"]; Incrop, [T"--"];
+       Binop, [T"+"]; Binop, [T"-"];
+       Num, [T"0"]; Num, [T"1"]; Num, [T"2"]; Num, [T"3"]; Num, [T"4"];
+       Num, [T"5"]; Num, [T"6"]; Num, [T"7"]; Num, [T"8"]; Num, [T"9"]])
+  = (Expr,
+     [Expr, [N Num];
+      Expr, [N Expr; N Binop; N Expr];
+      Incrop, [T"++"]; Incrop, [T"--"];
+      Binop, [T "+"]; Binop, [T "-"];
+      Num, [T "0"]; Num, [T "1"]; Num, [T "2"]; Num, [T "3"]; Num, [T "4"];
+      Num, [T "5"]; Num, [T "6"]; Num, [T "7"]; Num, [T "8"]; Num, [T "9"]])
+;;
+
+let awksub_test3 =
+  filter_blind_alleys (Expr, List.tl (List.tl (List.tl awksub_rules))) =
+    filter_blind_alleys (Expr, List.tl (List.tl awksub_rules))
+;;
 
 
-
+if debug then Printf.printf "awksub_test0: %B\n" awksub_test0;;
+if debug then Printf.printf "awksub_test1: %B\n" awksub_test1;;
+if debug then Printf.printf "awksub_test2: %B\n" awksub_test2;;
+if debug then Printf.printf "awksub_test3: %B\n" awksub_test3;;
 
 
 
