6e53cf9fdf12c1d4c90bbf08f39528d337c606e4
Vivek <vivek.a.sivakumar@gmail.com>
2016-02-02 23:59:20 -0800

    improved better safe

--- hw3/BetterSafeState.java
+++ hw3/BetterSafeState.java
@@ -1,16 +1,27 @@
-import java.util.concurrent.*;
+import java.util.concurrent.locks.ReentrantLock;
 class BetterSafeState implements State {
     private byte[] value;
     private byte maxval;
+    private ReentrantLock[] lockArr;
+    private final ReentrantLock swapLock = new ReentrantLock();
+
+    private void initLockArr(){
+        lockArr = new ReentrantLock[value.length];
+        for (int i = 0; i < value.length; i++){
+            lockArr[i] = new ReentrantLock();
+        }
+    }
 
     BetterSafeState(byte[] v) { 
-        value = v; 
+        value = v;
+        //initLockArr(); 
         maxval = 127;
     }
 
     BetterSafeState(byte[] v, byte m) { 
-        value = v; 
-        maxval = m; 
+        value = v;
+        //initLockArr(); 
+        maxval = m;
     }
 
     public int size() { 
@@ -21,12 +32,27 @@ class BetterSafeState implements State {
         return value; 
     }
 
-    public synchronized boolean swap(int i, int j) {
-    	if (value[i] <= 0 || value[j] >= maxval) {
-    	    return false;
-    	}
-    	value[i]--;
-    	value[j]++;
-    	return true;
+    public boolean swap(int i, int j) {
+        while(true){
+            if (swapLock.tryLock()){
+            // if (lockArr[i].tryLock() && lockArr[j].tryLock()){
+                if (value[i] <= 0 || value[j] >= maxval) {
+                    // lockArr[i].unlock();
+                    // lockArr[j].unlock();
+                    swapLock.unlock();
+                    return false;
+                }
+        	    value[i]--;
+        	    value[j]++;
+                swapLock.unlock();
+                // lockArr[i].unlock();
+                // lockArr[j].unlock();  
+                return true;
+            }
+            // else {
+            //     if (lockArr[i].isHeldByCurrentThread()) lockArr[i].unlock();
+            //     if (lockArr[j].isHeldByCurrentThread()) lockArr[j].unlock();
+            // }
+        }
     }
 }
