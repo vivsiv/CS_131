8a50f90ef8ecf883b8e7ebb383b736507853b131
Vivek <vivek.a.sivakumar@gmail.com>
2016-02-05 18:03:11 -0800

    clean up better safe

--- hw3/BetterSafeState.java
+++ hw3/BetterSafeState.java
@@ -2,25 +2,15 @@ import java.util.concurrent.locks.ReentrantLock;
 class BetterSafeState implements State {
     private byte[] value;
     private byte maxval;
-    private ReentrantLock[] lockArr;
     private final ReentrantLock swapLock = new ReentrantLock();
-
-    private void initLockArr(){
-        lockArr = new ReentrantLock[value.length];
-        for (int i = 0; i < value.length; i++){
-            lockArr[i] = new ReentrantLock();
-        }
-    }
 
     BetterSafeState(byte[] v) { 
         value = v;
-        //initLockArr(); 
         maxval = 127;
     }
 
     BetterSafeState(byte[] v, byte m) { 
         value = v;
-        //initLockArr(); 
         maxval = m;
     }
 
@@ -35,24 +25,15 @@ class BetterSafeState implements State {
     public boolean swap(int i, int j) {
         while(true){
             if (swapLock.tryLock()){
-            // if (lockArr[i].tryLock() && lockArr[j].tryLock()){
                 if (value[i] <= 0 || value[j] >= maxval) {
-                    // lockArr[i].unlock();
-                    // lockArr[j].unlock();
                     swapLock.unlock();
                     return false;
                 }
         	    value[i]--;
         	    value[j]++;
                 swapLock.unlock();
-                // lockArr[i].unlock();
-                // lockArr[j].unlock();  
                 return true;
             }
-            // else {
-            //     if (lockArr[i].isHeldByCurrentThread()) lockArr[i].unlock();
-            //     if (lockArr[j].isHeldByCurrentThread()) lockArr[j].unlock();
-            // }
         }
     }
 }
