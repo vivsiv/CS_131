10c4bb106fc6cbccb4a1e1a63470e9e4bf7711fb
Vivek <vivek.a.sivakumar@gmail.com>
2016-02-17 00:56:39 -0800

    working FD ken ken

--- hw4/kenken.pl
+++ hw4/kenken.pl
@@ -2,18 +2,13 @@ kenken(N,C,T) :-
 	validBoard(N,T),
 	validRows(T),
 	validColumns(T),
-	validCages(T,C).
+	validCages(T,C),
+	constrainSolution(T).
 
 validBoard(N,T) :- 
-	validNumbers(N,T),
 	validNumRows(N,T),
-	validNumCols(N,T).
-
-validNumbers(N,T) :- 
-	maplist(validRowNumbers(N),T).
-
-validRowNumbers(N,Row) :-
-	fd_domain(Row,1,N).
+	validNumCols(N,T),
+	validNumbers(N,T).
 
 validNumRows(N,T) :- 
 	length(T,N).
@@ -23,6 +18,12 @@ validRowLength(N,Row) :-
 
 validNumCols(N,T) :- 
 	maplist(validRowLength(N),T).
+
+validNumbers(N,T) :- 
+	maplist(validRowNumbers(N),T).
+
+validRowNumbers(N,Row) :-
+	fd_domain(Row,1,N).
 
 validRows(T) :-
 	maplist(validRow,T).
@@ -34,6 +35,7 @@ validColumns(T) :-
 	transpose(T,Trans),
 	validRows(Trans).
 
+%% SWI Prolog Transpose implementation
 transpose([], []).
 transpose([F|Fs], Ts) :-
     transpose(F, [F|Fs], Ts).
@@ -50,7 +52,6 @@ lists_firsts_rests([[F|Os]|Rest], [F|Fs], [Os|Oss]) :-
 validCages(T,C) :-
 	maplist(validCage(T),C).
 
-
 validCage(T,Cage) :-
 	matchCage(T,Cage).
 
@@ -61,39 +62,42 @@ matchCage(T,*(P,L)) :-
 	multiply(P,L,T,1).
 
 matchCage(T,-(D,J,K)) :- 
-	subtract(D,J,K,T);
-	subtract(D,K,J,T).
+	subtract(D,J,K,T).
 
 matchCage(T,/(Q,J,K)) :-
-	divide(Q,J,K,T);
-	divide(Q,K,J,T).
+	divide(Q,J,K,T).
 
 add(Sum,[],_,Sum).
 add(Sum,[Label|Tail],T,Acc) :-
 	getElement(Label,T,Elem),
-	NAcc is Acc + Elem,
+	NAcc #= Acc + Elem,
 	add(Sum,Tail,T,NAcc).
 
 
 multiply(Product,[],_,Product).
 multiply(Product,[Label|Tail],T,Acc) :-
 	getElement(Label,T,Elem),
-	NAcc is Acc * Elem,
+	NAcc #= Acc * Elem,
 	multiply(Product,Tail,T,NAcc).
 
 subtract(Difference,J,K,T) :-
 	getElement(J,T,E1),
 	getElement(K,T,E2),
-	Difference is E1 - E2.
+	Difference #= E1 - E2;
+	Difference #= E2 - E1.
 
 divide(Quotient,J,K,T) :-
 	getElement(J,T,E1),
 	getElement(K,T,E2),
-	Quotient is E1//E2.
+	Quotient #= E1//E2;
+	Quotient #= E2//E1.
 
 getElement(I-J,T,Elem) :-
 	nth(I,T,Row),
 	nth(J,Row,Elem).
+
+constrainSolution(T) :-
+	maplist(fd_labeling,T).
 
 add(10,[1-4,2-2,3-1],[[2,1,3,4],[1,2,3,4],[4,3,2,1],[3,1,2,4]],0).
 multiply(32,[1-4,2-2,3-1],[[2,1,3,4],[1,2,3,4],[4,3,2,1],[3,1,2,4]],1).
@@ -106,8 +110,52 @@ validCage([[2,1,3,4],[1,2,3,4],[4,3,2,1],[3,1,2,4]],-(2,2-2,1-4)).
 validCage([[2,1,3,4],[1,2,3,4],[4,3,2,1],[3,1,2,4]],/(2,1-4,2-2)).
 validCage([[2,1,3,4],[1,2,3,4],[4,3,2,1],[3,1,2,4]],/(2,2-2,1-4)).
 
+kenken_testcase(
+  6,
+  [
+   +(11, [1-1, 2-1]),
+   /(2, 1-2, 1-3),
+   *(20, [1-4, 2-4]),
+   *(6, [1-5, 1-6, 2-6, 3-6]),
+   -(3, 2-2, 2-3),
+   /(3, 2-5, 3-5),
+   *(240, [3-1, 3-2, 4-1, 4-2]),
+   *(6, [3-3, 3-4]),
+   *(6, [4-3, 5-3]),
+   +(7, [4-4, 5-4, 5-5]),
+   *(30, [4-5, 4-6]),
+   *(6, [5-1, 5-2]),
+   +(9, [5-6, 6-6]),
+   +(8, [6-1, 6-2, 6-3]),
+   /(2, 6-4, 6-5)
+  ]
+).
 
-
+kenken(
+  6,
+  [
+   +(11, [1-1, 2-1]),
+   /(2, 1-2, 1-3),
+   *(20, [1-4, 2-4]),
+   *(6, [1-5, 1-6, 2-6, 3-6]),
+   -(3, 2-2, 2-3),
+   /(3, 2-5, 3-5),
+   *(240, [3-1, 3-2, 4-1, 4-2]),
+   *(6, [3-3, 3-4]),
+   *(6, [4-3, 5-3]),
+   +(7, [4-4, 5-4, 5-5]),
+   *(30, [4-5, 4-6]),
+   *(6, [5-1, 5-2]),
+   +(9, [5-6, 6-6]),
+   +(8, [6-1, 6-2, 6-3]),
+   /(2, 6-4, 6-5)
+  ],[[5,6,3,4,1,2],
+     [6,1,4,5,2,3],
+     [4,5,2,3,6,1],
+     [3,4,1,2,5,6],
+     [2,3,6,1,4,5],
+     [1,2,5,6,3,4]]
+).
 %% validCages(N,C,T).
 
 %% plain_kenken(N,C,T).
