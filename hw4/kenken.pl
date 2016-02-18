843d15fdc591c6d035ece2d49eac12d90b073a97
Vivek <vivek.a.sivakumar@gmail.com>
2016-02-17 20:06:58 -0800

    working plain ken ken

--- hw4/kenken.pl
+++ hw4/kenken.pl
@@ -156,16 +156,158 @@ kenken(
      [2,3,6,1,4,5],
      [1,2,5,6,3,4]]
 ).
-%% validCages(N,C,T).
 
-%% plain_kenken(N,C,T).
+plain_kenken(N,C,T) :-
+	validBoard_P(N,T),
+	validRows_P(N,T),
+	validColumns_P(N,T),
+	validCages_P(T,C).
 
-%% nList(N,N,[N]).
-%% nList(Start,End,[Head|Tail]) :-
-%% 	Start =< End,
-%% 	Start = Head,
-%% 	NewStart is Start + 1,
-%% 	nList(NewStart,End,Tail).
+validBoard_P(N,T) :-
+	validNumRows(N,T),
+	validNumCols(N,T).
+
+nList(N,N,[N]).
+nList(Start,End,[Head|Tail]) :-
+	Start =< End,
+	Start = Head,
+	NewStart is Start + 1,
+	nList(NewStart,End,Tail).
+
+validRows_P(N,T) :-
+	nList(1,N,NList),
+	maplist(validRowNumbers_P(NList),T).
+
+validRow_P(Row) :-
+	permutation(NList,Row).
+
+validColumns_P(T) :-
+	transpose(T,Trans),
+	validColumn_P(Trans).
+
+uniqueList([]).
+uniqueList([Head|Tail]) :- \+ member(Head,Tail), uniqueList(Tail).
+
+validColumn_P(T) :-
+	maplist(uniqueList,T).
+
+validCages_P(T,C) :-
+	maplist(validCage_P(T),C).
+
+validCage_P(T,Cage) :-
+	matchCage_P(T,Cage).
+
+matchCage_P(T,+(S,L)) :-
+	add_P(S,L,T,0).
+
+matchCage_P(T,*(P,L)) :-
+	multiply_P(P,L,T,1).
+
+matchCage_P(T,-(D,J,K)) :- 
+	subtract_P(D,J,K,T);
+	subtract_P(D,K,J,T).
+
+matchCage_P(T,/(Q,J,K)) :-
+	divide_P(Q,J,K,T);
+	divide_P(Q,K,J,T).
+
+add_P(Sum,[],_,Sum).
+add_P(Sum,[Label|Tail],T,Acc) :-
+	getElement(Label,T,Elem),
+	NAcc is Acc + Elem,
+	add_P(Sum,Tail,T,NAcc).
+
+
+multiply_P(Product,[],_,Product).
+multiply_P(Product,[Label|Tail],T,Acc) :-
+	getElement(Label,T,Elem),
+	NAcc is Acc * Elem,
+	multiply_P(Product,Tail,T,NAcc).
+
+subtract_P(Difference,J,K,T) :-
+	getElement(J,T,E1),
+	getElement(K,T,E2),
+	Difference is E1 - E2.
+
+divide_P(Quotient,J,K,T) :-
+	getElement(J,T,E1),
+	getElement(K,T,E2),
+	Quotient is E1//E2.
+
+
+add_P(10,[1-4,2-2,3-1],[[2,1,3,4],[1,2,3,4],[4,3,2,1],[3,1,2,4]],0).
+multiply_P(32,[1-4,2-2,3-1],[[2,1,3,4],[1,2,3,4],[4,3,2,1],[3,1,2,4]],1).
+subtract_P(2,1-4,2-2,[[2,1,3,4],[1,2,3,4],[4,3,2,1],[3,1,2,4]]).
+divide_P(2,1-4,2-2,[[2,1,3,4],[1,2,3,4],[4,3,2,1],[3,1,2,4]]).
+validCage_P([[2,1,3,4],[1,2,3,4],[4,3,2,1],[3,1,2,4]],+(10,[1-4,2-2,3-1])).
+validCage_P([[2,1,3,4],[1,2,3,4],[4,3,2,1],[3,1,2,4]],*(32,[1-4,2-2,3-1])).
+validCage_P([[2,1,3,4],[1,2,3,4],[4,3,2,1],[3,1,2,4]],-(2,1-4,2-2)).
+validCage_P([[2,1,3,4],[1,2,3,4],[4,3,2,1],[3,1,2,4]],-(2,2-2,1-4)).
+validCage_P([[2,1,3,4],[1,2,3,4],[4,3,2,1],[3,1,2,4]],/(2,1-4,2-2)).
+validCage_P([[2,1,3,4],[1,2,3,4],[4,3,2,1],[3,1,2,4]],/(2,2-2,1-4)).
+
+plain_kenken_testcase(
+  4,
+  [
+   +(8, [1-1,2-1,3-1]),
+   -(2, 1-2, 1-3),
+   /(2, 1-4, 2-4),
+   /(2, 2-2, 2-3),
+   *(24, [3-2,4-1,4-2]),
+   -(2, 3-3, 4-3),
+   *(3, [3-4, 4-4])
+  ]
+).
+
+plain_kenken(
+  4,
+  [
+   +(8, [1-1,2-1,3-1]),
+   -(2, 1-2, 1-3),
+   /(2, 1-4, 2-4),
+   /(2, 2-2, 2-3),
+   *(24, [3-2,4-1,4-2]),
+   -(2, 3-3, 4-3),
+   *(3, [3-4, 4-4])
+  ],[[4,1,3,2],
+  	[3,2,1,4],
+  	[1,4,2,3],
+  	[2,3,4,1]]
+).
+
+plain_kenken_testcase2(
+	3,
+	[
+     *(2, [1-1,2-1,2-2]),
+     -(1, 1-2, 1-3),
+     -(1, 3-1, 3-2),
+     *(3, [2-3,3-3])
+   ]
+).
+
+plain_kenken(
+	3,
+	[
+     *(2, [1-1,2-1,2-2]),
+     -(1, 1-2, 1-3),
+     -(1, 3-1, 3-2),
+     *(3, [2-3,3-3])
+   ], [[1,3,2],
+   	  [2,1,3],
+   	  [3,2,1]]
+).
+
+%% plain_kenken_testcase2(
+%% 	3,
+%% 	[
+%%      *(2, [1-1,2-1,2-2]),
+%%      -(1, 1-2, 1-3),
+%%      -(1, 3-1, 3-2),
+%%      *(3, [3-1,3-2]),
+%%   ]
+%% ).
+
+
 
 %% permNList(N,List) :-
 %% 	nList(1,N,Nlist),
@@ -180,8 +322,5 @@ kenken(
 %% validColumns(N,T) :-
 %% 	transpose(T,Trans),
 %% 	validRows(N,Trans).
-
-kk_test0(1,[]).
-kk_test1(2,[]).
 
 %% validRow(4,R).
